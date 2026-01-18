d5993fb1312863c974019a85e0b891e67ebc0836
Zinedine Chami <zinedine.chami@gmail.com>
2026-01-18 13:08:21 +0100

    Update icon and modify Slideshow component: Replaced the application icon and adjusted the indentation in the Slideshow component for improved code readability. Updated contact email in content.json for better communication.

--- app/components/Slideshow/Slideshow.tsx
+++ app/components/Slideshow/Slideshow.tsx
@@ -30,14 +30,14 @@ export default function Slideshow({ images, currentIndex }: SlideshowProps) {
   return (
     <div className="w-full border-[0.5px] border-zinc-300 bg-zinc-50/50 rounded-[6px] overflow-hidden relative cursor-default">
       <div className="relative w-full aspect-[16/10]">
-        <Image
-          src={images[currentIndex].src}
-          alt={images[currentIndex].alt}
-          fill
-          className="object-cover"
-          priority
-          quality={100}
-        />
+            <Image
+              src={images[currentIndex].src}
+              alt={images[currentIndex].alt}
+              fill
+              className="object-cover"
+              priority
+              quality={100}
+            />
       </div>
     </div>
   );
